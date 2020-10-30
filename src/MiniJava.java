4b13ae604519f16a234336804719388642b60167
Karamvir Rai <krai96@uw.edu>
2020-10-29 17:10:10 -0700

    small step for man

--- src/MiniJava.java
+++ src/MiniJava.java
@@ -1,8 +1,12 @@
 import Scanner.*;
-import Parser.sym;
+import Parser.*;
+import AST.*;
+import AST.Visitor.*;
 import java_cup.runtime.Symbol;
 import java_cup.runtime.ComplexSymbolFactory;
 import java.io.*;
+import java.util.*;
+
 
 public class MiniJava {
     public static void main(String[] args) {
@@ -13,12 +17,23 @@ public class MiniJava {
             InputStream inputStream = new FileInputStream(filePath); 
             Reader in = new BufferedReader(new InputStreamReader(inputStream));
             scanner s = new scanner(in, sf);
+            parser p = new parser(s, sf);
+            Symbol root = p.parse();
+
+            @SuppressWarnings("unchecked")
+            List<Statement> program = (List<Statement>)root.value;
+            for (Statement statement: program) {
+                statement.accept(new PrettyPrintVisitor());
+                System.out.print("\n");
+            }
+            /*
             Symbol t = s.next_token();
             while (t.sym != sym.EOF) { 
                 // print each token that we scan
                 System.out.print(s.symbolToString(t) + " ");
                 t = s.next_token();
             }
+            */
             System.exit(0);
         } catch (Exception e) {
             // yuck: some kind of error in the compiler implementation
