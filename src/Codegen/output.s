d02f2ba01ed449921721c27aa470f3f0eed861b9
Julian Gallegos <julianga@uw.edu>
2020-12-01 13:07:50 -0800

    functioning output.s

--- src/Codegen/output.s
+++ src/Codegen/output.s
@@ -3,24 +3,27 @@
 asm_main:
 	movq $8, %rdi
 	call mjcalloc
-	leaq Person$$(%rip), %rdx
-	movq %rdx, 0(%rax)
-	movq 0(%rbp), %rdi
-	movq 0(%rdi), %rax
+	leaq Person$$, %rdx
+	movq %rdx, 0(%rax) #at the end of this, put pointer in rax
+        
+        movq %rax, %rdi
+	movq 0(%rax), %rax
 	call *8(%rax)
 	movq %rax, %rdi
 	call put
-	.data
+	ret
 Person$testA:
 	movq $401, %rax
 	movq %rax, %rdi
 	call put
 	movq $1, %rax
+	ret
 Person$testB:
         movq $2020, %rax
         movq %rax, %rdi
         call put
         movq $1, %rax
+	ret
 	.data
 Person$$: .quad 0
         .quad Person$testA
