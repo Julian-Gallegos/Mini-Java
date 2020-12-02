f6520c857535e6bfc9e91691ae04f605ab9a1aaf
Karamvir Rai <krai96@attu1.cs.washington.edu>
2020-12-02 00:05:50 -0800

    daily progress

--- SamplePrograms/Test.java
+++ SamplePrograms/Test.java
@@ -1,16 +1,16 @@
 class Test{
     public static void main(String[] a) {
-        System.out.println(new Person().incrementAge());
+        System.out.println(new Person().incrementAge(40));
     }
 }
 
 class Person {
-    public int incrementAge() {
+    public int incrementAge(int age) {
         int i;
         int j;
         i = 30;
-        j = i;
-        System.out.print(j);
+        j = i + age;
+        System.out.println(j);
         return 1;
     }
 
