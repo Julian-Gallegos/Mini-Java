28a49b82631cb3ce0148837cdf9a013b8190f63f
Karamvir Rai <krai96@uw.edu>
2020-12-05 13:14:22 -0800

    early morning fix

--- src/SemanticsAndTypes/ErrorCheckVisitor.java
+++ src/SemanticsAndTypes/ErrorCheckVisitor.java
@@ -134,12 +134,13 @@ public class ErrorCheckVisitor implements Visitor {
         n.t.accept(this);   // return type
 
         for ( int i = 0; i < n.fl.size(); i++ ) {
-	    
-	    String idLookup = lookupTypeForID(n.fl.get(i).i.s);
-	    if (idLookup != null && !symbolTable.globalScope.containsKey(idLookup)) {
+
+	        String idLookup = lookupTypeForID(n.fl.get(i).i.s);
+	        if (idLookup != null && !typeTable.types.containsKey(idLookup)) {
                 System.out.println("Error: (Line " + n.fl.line_number + ") argument type " + idLookup + " is not defined.");
                 errorCounter++;
             }
+
             n.fl.get(i).accept(this);
         }
         for ( int i = 0; i < n.vl.size(); i++ ) {
