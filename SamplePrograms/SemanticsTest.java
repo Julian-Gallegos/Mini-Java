19d657986a7ae3161327cdf53c4ec9a23564f79e
Karamvir Rai <krai96@uw.edu>
2020-12-05 00:53:59 -0800

    test files

--- SamplePrograms/SemanticsTest.java
+++ SamplePrograms/SemanticsTest.java
@@ -1,11 +1,11 @@
 class SemanticsTest {
     public static void main(String[] args) {
-        System.out.println("Should not be allowed.");
+        System.out.println(new Test().testMethod());
     }
 }
 
 class Test {
-    public void testMethod() {
-
+    public int testMethod(int a) {
+        return a;
     }
 }
--- SamplePrograms/Test.java
+++ SamplePrograms/Test.java
@@ -1,48 +1,24 @@
 class Test{
     public static void main(String[] a) {
-        System.out.println(new Person().testA(200));
+        System.out.println(new B().test());
     }
 }
 
-class Person {
- 
-    public int testA(int w) {
-	BankAccount u;
-	u = new BankAccount();
-	return u.increment(w); 
-    }
- 
-    public int controlFlowTest() {
-	int a;
-	int b;
-	b = 7;
-	a = 1;
-	while (a < 10 && !(b < a)) {
-	    a = a + 1; 
-	}
-	return a; 
+class A {
+    int a;
+
+    public int test() {
+        return 2020;
     }
 
-    public int incrementAge(int age) {
-        int i;
-        int j;
-        i = 30;
-        j = i + age;
-        System.out.println(j);
-        return 1;
-    }
-
-    public int testB() {
-        System.out.println(2020);
-	return 1;
+    public int getNum() {
+        return 10;
     }
 }
 
-class BankAccount {
-    int balance;
-
-    public int increment(int i) {
-	balance = i;
-	return balance + 500; 
+class B extends A {
+    public int test() {
+        a = 100;
+        return a + this.getNum() + getNum();
     }
 }
