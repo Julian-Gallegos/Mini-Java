253723780796ee0f4019e71b7c73d850f9a9ea24
Karamvir Rai <krai96@attu2.cs.washington.edu>
2020-12-05 14:39:42 -0800

    error test file

--- SamplePrograms/SemanticsTest.java
+++ SamplePrograms/SemanticsTest.java
@@ -1,16 +1,23 @@
 class SemanticsTest {
     public static void main(String[] args) {
-        System.out.println(new Test().testMethod(3, 10));
+        System.out.println(new Test().testMethod(3));
     }
 }
 
-class A {
-    int a;
+class C {
+    int a; 
+}
+
+class A extends C {
+    int b; 
 }
 
 class Test extends A {
-    int a;
+
     public int testMethod(int a) {
-        return 10;
+	int b;
+	b = 20; 
+	return b;
     }
+    
 }
