229b5427a1f3c059c9cd935be947c84301448d69
Karamvir Rai <krai96@attu8.cs.washington.edu>
2020-12-05 22:10:35 -0800

    aosdifjasdfioajsdfiosj

--- SamplePrograms/Test.java
+++ SamplePrograms/Test.java
@@ -1,17 +1,23 @@
 class Test {
     public static void main(String[] a) {
-	System.out.println(new Two().getIt(new One().hello()));
-    }
-}
-
-class One {
-    public int hello() {
-	return 20; 
+	System.out.println(new Two().hi());
     }
 }
 
 class Two {
-    public int getIt(int a) {
-	return a + 10; 
+    public int hi() {
+	boolean out;
+	int a; 
+	out = true;
+        a = 0; 
+
+	while (out) {
+	    if (10 < a) {
+		out = true; 
+	    } else {
+		a = a + 1;
+	    }
+	}
+	return a; 
     }
 }
